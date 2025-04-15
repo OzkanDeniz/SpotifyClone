649bcb63fa57b7bf60bc4e57bcf99b9d86e41086
ozkan <ozkandnz09@gmail.com>
2025-04-15 13:40:55 +0300

    shadcn ui button

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,3 +1,4 @@
+import { Button } from "./components/ui/button"
 
 function App() {
   
@@ -5,6 +6,7 @@ function App() {
   return (
     <>
       <h1 className="text-red-500 text-5xl">Hello</h1>
+      <Button variant={"outline"}>This is a button</Button>
     </>
   )
 }
