bf65087cde7fcfb1e1d007eef2dc09b22fca6a16
ozkan <ozkandnz09@gmail.com>
2025-04-15 17:40:49 +0300

    await

--- backend/lib/db.js
+++ backend/lib/db.js
@@ -1,5 +1,9 @@
-import mongoose from "mongoose"
+import mongoose, { mongo } from "mongoose"
 
 export const connectDB = async ()=>{
-    
+    try {
+        await mongoose.connect(process.env.MONGODB_URI)
+    } catch (error) {
+        
+    }
 }
