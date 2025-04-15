bef73cdead0ce3edf881f17f5e0f2d8cbf8be05b
ozkan <ozkandnz09@gmail.com>
2025-04-15 15:26:34 +0300

    script

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,5 +7,5 @@ const app = express();
 const PORT = process.env.PORT;
 
 app.listen(PORT, () => {
-  console.log("Server is running on port 5000" + PORT);
+  console.log("Server is running on port " + PORT);
 });
