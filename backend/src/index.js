3eb7770e279df7d8a028b5af65cf30f4b981381c
ozkan <ozkandnz09@gmail.com>
2025-04-15 15:39:55 +0300

    router

--- backend/src/index.js
+++ backend/src/index.js
@@ -6,6 +6,10 @@ dotenv.config();
 const app = express();
 const PORT = process.env.PORT;
 
+app.use("/",(req,res)=>{
+    res.send("")
+})
+
 app.listen(PORT, () => {
   console.log("Server is running on port " + PORT);
 });
